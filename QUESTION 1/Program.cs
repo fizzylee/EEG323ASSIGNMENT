b318b01495f03f39e75dd86506b254df9d95255c
fizzylee <43205140+fizzylee@users.noreply.github.com>
2021-07-15 22:37:35 +0100

    Add files via upload

---
+++ QUESTION 1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace WEEK_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { string course;
+          string studentProgram ;
+        
+         
+
+           Console.WriteLine("Are you a full-time student or a part-time student?");
+
+           studentProgram = Console.ReadLine();
+
+           if (studentProgram == "full-time")
+           {
+               Console.WriteLine("What is your course of study?");
+               course =  Console.ReadLine();
+
+               Console.WriteLine($"{course} is a good course");
+
+           }
+
+           else if (studentProgram == "part-time")
+           {
+               Console.WriteLine("How many credits are you taking?");
+
+          int credits= Convert.ToInt32(Console.ReadLine());
+           
+           if (credits > 6)
+        Console.WriteLine("Too much units for a part-time student");
+
+        else{
+             Console.WriteLine("Proceed");
+        }
+           }
+
+
+
+
+           
+        }
+    }
+}
---
+++ QUESTION 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TEST_ANSWER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   Console.WriteLine("Enter your values for a, b, c, d, e, f");
+
+            Console.WriteLine("a : ");
+            double a = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("b : ");
+            double b = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("c : ");
+            double c = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("d : ");
+          double d = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("e : ");
+            double e = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("f : ");
+            double f = Convert.ToInt32(Console.ReadLine()); 
+
+
+            double X = (e * d - b * f )/(a * d - b * c);
+            double Y = (a * f - e * c) / (a * d - b * c);
+
+            if (a * d - b * c == 0) {
+                Console.WriteLine("The Equation has no solution");
+            } else {
+                Console.WriteLine("X is " + X);
+                Console.WriteLine("Y is " + Y);
+            }
+
+        }
+    }
+}
